b52bb51800fd7cf3758c85b750ed0a827e0edb50
Dr. Ángel Vásquez <angel.vasquez@tecnologiasmexico.com>
2020-10-22 09:28:00 -0500

    Se agregó un formulario para solicitud de datos

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Práctica Utilizando GIT</title>
+<meta charset="UTF-8">
+<meta name="description" content="GIT, PHP, HTML5">
+<meta name="keywords" content="GIT, PHP, HTML5">
+<meta name="author" content="Dr. Angel Vasquez">
+<link rel="stylesheet" href="css/estilo.css" type="text/css">
+
+</head>
+<body>
+    <header>
+        <nav>PRÁCTICA UTILIZANDO GIT</nav>
+    </header>
+    <form action="" name="v_f" id="fid" method="POST">
+        <input type="text" name="v_nom" maxlength="30" placeholder="Nombre(s)">
+        <input type="text" name="v_apellidos" maxlength="50" placeholder="Apellidos">
+        <input type="number" name="v_edad" min="18" max="25" placeholder="Edad">
+        <input type="submit" name="v_enviar" value="Enviar Información">
+    </form>
+<footer>
+    <?php 
+        echo "Derechos Reservados ".date("Y");
+    ?>
+</footer>
+</body>
+</html>
